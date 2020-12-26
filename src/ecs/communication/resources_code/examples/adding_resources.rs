0953907525df8b1f7d97a82528a7cd9b3fcb1de7
Alice Cecile <alice.i.cecile@gmail.com>
2020-12-26 00:32:20 -0500

    Cargo fmt

--- src/ecs/communication/resources_code/examples/adding_resources.rs
+++ src/ecs/communication/resources_code/examples/adding_resources.rs
@@ -1,29 +1,32 @@
 use bevy::prelude::*;
 
-struct InfantryStats{
-	hp: f32,
-	damage: f32,
-	speed: f32,
+struct InfantryStats {
+    hp: f32,
+    damage: f32,
+    speed: f32,
 }
 
-struct PlayerResources{
-	gold: usize,
-	wood: usize,
+struct PlayerResources {
+    gold: usize,
+    wood: usize,
 }
 
 struct InitialPosition(f32, f32);
 
-enum PlayerColor{
-	Red,
-	Blue,
-	Pink
+enum PlayerColor {
+    Red,
+    Blue,
+    Pink,
 }
 
 fn main() {
-	App::build()
-	.add_resource::<InfantryStats>()
-	.init_resource(PlayerResources{gold: 1000, wood: 500})
-	.init_resource(InitialPosition(0,0))
-	.init_resource(PlayerColor::Pink)
-	.run();
+    App::build()
+        .add_resource::<InfantryStats>()
+        .init_resource(PlayerResources {
+            gold: 1000,
+            wood: 500,
+        })
+        .init_resource(InitialPosition(0, 0))
+        .init_resource(PlayerColor::Pink)
+        .run();
 }
--- src/ecs/communication/resources_code/examples/resource_smart_pointers.rs
+++ src/ecs/communication/resources_code/examples/resource_smart_pointers.rs
@@ -1,2 +1,3 @@
 use bevy::prelude::*;
 
+struct Score(u32, u32);
